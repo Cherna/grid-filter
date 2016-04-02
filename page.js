e49c0ab1496a8c16e4e192ce1f3437e492a6691c
Tomas Chernov <Chernatom@gmail.com>
2016-04-02 19:23:57 -0300

    Styled landing page.

---
+++ page.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+  var filter = new $.SimpleGridFilter({
+    'input': '#first-example-input',
+    'container': '.simple-table-1',
+    'row': '.table-row-1',
+    'matchContainer': '.name',
+    'matchText': ''
+  });
+
+})
